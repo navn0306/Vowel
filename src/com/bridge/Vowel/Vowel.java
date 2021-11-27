14a8903f8cc0304c39e722dcc14b6dbc70f2f6e0
Navneet <navn0306@gmail.com>
2021-11-28 01:32:58 +0530

    ADD: Added welcome message for the program

---
+++ src/com/bridge/Vowel/Vowel.java
@@ -0,0 +1,7 @@
+package com.bridge.Vowel;
+
+public class Vowel {
+    public static void main(String[] args) {
+        System.out.println("\n Welcome, This program checks if the given input is vowel or consonant.");
+    }
+}
